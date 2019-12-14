550fc6bf433a9ef6f1b8c37752afdfa176fbb405
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-12-14 18:42:38 +0600

    Update

--- fb.js
+++ fb.js
@@ -1,6 +1,44 @@
 // document.getElementById('rightCol').style.display = 'none';
-document.getElementById('stream_pagelet').style.display = 'none';
-document.getElementById('rhc_col').style.display = 'none';
-document.getElementById('pagelet_ego_pane').style.display = 'none';
+// document.getElementById('stream_pagelet').style.display = 'none';
+// document.getElementById('rhc_col').style.display = 'none';
+// document.getElementById('pagelet_ego_pane').style.display = 'none';
+// document.getElementsByClassName('img').style.display = 'none';
 // document.getElementsByClassName('pagelet_ego_pane').style.display = 'none'; CHAT SIDEBAR PROFILE PAGE -> NOT WORKING
 
+
+
+
+
+
+
+
+
+
+
+// I've tried to remove images from chat side bar but failed. code below
+// window.addEventListener("load", myMain, false);
+
+
+// console.log(document.getElementsByClassName('img'));
+
+// var chatProfileImages = document.getElementsByClassName('img');
+// for (var i = 0; i < chatProfileImages.length; ++i) {
+    
+//     var item = chatProfileImages[i];
+//     console.log(item);
+//     item.style.display = 'none';
+// }
+
+// function myMain(evt) {
+//     console.log(document.getElementsByClassName('img'));
+
+//     var chatProfileImages = document.getElementsByClassName('img');
+//     for (var i = 0; i < chatProfileImages.length; ++i) {
+        
+//         var item = chatProfileImages[i];
+//         console.log(item);
+//         item.style.display = 'none';
+//     }
+
+//     // document.getElementsByClassName('img').style.display = 'none';
+// }
