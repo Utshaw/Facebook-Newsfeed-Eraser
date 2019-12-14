a8b6065aecd89679c4822ae260e5ba430bc98e2d
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-12-14 12:43:10 +0600

    From homepage -> Newsfeed, Friend request section removed; inside group-> annoying recent group photos removed ;From homepage, group page-> group recommendation removed

--- fb.js
+++ fb.js
@@ -1,1 +1,5 @@
-document.getElementById('contentCol').style.display = 'none';
+// document.getElementById('rightCol').style.display = 'none';
+document.getElementById('stream_pagelet').style.display = 'none';
+document.getElementById('rhc_col').style.display = 'none';
+document.getElementById('pagelet_ego_pane').style.display = 'none';
+
