99d158e496c5c1045c8fdef8bb09ac114caa9f7b
Utshaw <farhan.tanvir.utshaw@gmail.com>
2019-12-14 18:02:33 +0600

    Update

--- fb.js
+++ fb.js
@@ -2,4 +2,5 @@
 document.getElementById('stream_pagelet').style.display = 'none';
 document.getElementById('rhc_col').style.display = 'none';
 document.getElementById('pagelet_ego_pane').style.display = 'none';
+// document.getElementsByClassName('pagelet_ego_pane').style.display = 'none'; CHAT SIDEBAR PROFILE PAGE -> NOT WORKING
 
